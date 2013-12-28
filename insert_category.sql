1fc1412aa6eaefb250f0d8c355d3db79cfa928bf
remlide <engremlide@gmail.com>
2013-12-28 13:26:11 +0800

    [commit] insert category

---
+++ insert_category.sql
@@ -0,0 +1,9 @@
+﻿INSERT INTO category VALUES('lovestory');
+INSERT INTO category VALUES('friction');
+INSERT INTO category VALUES('action');
+INSERT INTO category VALUES('adventure');
+INSERT INTO category VALUES('animation');
+INSERT INTO category VALUES('biography');
+INSERT INTO category VALUES('comedy');
+INSERT INTO category VALUES('crime');
+IINSERT INTO category VALUES('drama');NSERT INTO category VALUES('horror');
